eabe23abbbf45f20b0c0947f5e2679d7ce5e0ebc
Thomas Dowrick <tdowrick@gmail.com>
2015-11-12 16:13:05 +0000

    Real time data acquisiton and display working in MATLAB

--- Biosemi_tcp.m
+++ Biosemi_tcp.m
@@ -11,46 +11,54 @@ N_CHANNELS = 8;
 BYTES_PER_SAMPLE = 3;
 SAMPLES_PER_PACKET = BioSemi_Buffer /(N_CHANNELS*BYTES_PER_SAMPLE);
 
+%Connect to Biosemi over TCP
 biosemi_tcp_obj = create_tcp_obj(BioSemi_IP,BioSemi_Port,BioSemi_Buffer);
-
 
 fclose(biosemi_tcp_obj)
 fopen(biosemi_tcp_obj)
 
 clear data
 
+%No. of samples read so far
 SAMPLES_READ = 0;
 
-
+%Using this to measure run time
 T = [];
 tic
-while(SAMPLES_READ < Fs*10)
-    %pre allocate for speed
+while(SAMPLES_READ < Fs*100)
     
+    %pre allocate for speed
     if exist('data','var')
         data =[data zeros(N_CHANNELS,SAMPLES_PER_PACKET)];
     else
         data= zeros(N_CHANNELS,SAMPLES_PER_PACKET);
     end
     
+    %Get block of data from Biosemi
     data(:,(SAMPLES_READ+1):SAMPLES_READ+SAMPLES_PER_PACKET) = ...
         get_tcp_packet_from_BioSemi(biosemi_tcp_obj, N_CHANNELS, SAMPLES_PER_PACKET, BYTES_PER_SAMPLE);
     
     SAMPLES_READ = SAMPLES_READ + SAMPLES_PER_PACKET;
     
-    if ~mod(SAMPLES_READ,Fs)
+    %% Plotting stuff
+    if ~mod(SAMPLES_READ,Fs/10)
         
         T = [T toc];
         tic
         
-        for i = 1:2
-        subplot(2,1,i)
+        for i = 1:8
+        subplot(8,2,2*i-1)
         plot(data(i,:))
         
         end
+              
         drawnow
     end
     
+    if ~mod(SAMPLES_READ,2*Fs)
+        subplot(1,2,2)
+        pwelch(data(1,SAMPLES_READ-Fs:SAMPLES_READ),Fs,.95,[],Fs)
+    end
 end
 
 mean(T)
--- get_tcp_packet_from_BioSemi.m
+++ get_tcp_packet_from_BioSemi.m
@@ -1,32 +1,29 @@
 function data = get_tcp_packet_from_BioSemi (tcp_obj, N_CHANNELS, SAMPLES_PER_PACKET, BYTES_PER_SAMPLE)
 
 offset = 0;             %Byte offset corresponding to chan/sample being processed
+
+%The 24-bit biosemi data is transmitted as 3 bytes, which need to be
+%combined into one 32-bit value. Apply this bit mask to each set of 3 bytes
+%and sum them to get the actual data value.
+Bit_Shift_Mask = [8 16 24];
+
+%Scale factor to get values in uV
+MAGNITUDE_SCALE_FACTOR = 256*32;
+
+%Pre allocate space
 data = zeros(N_CHANNELS,SAMPLES_PER_PACKET);
+
+%Read TCP data
 stream = fread(tcp_obj);
 
-for sample = 1:SAMPLES_PER_PACKET
+%Process TCP data to voltages
+new_data = bitshift(stream,repmat( Bit_Shift_Mask,1,length(stream)/3)');
+shaped_data = reshape(new_data,3,SAMPLES_PER_PACKET*N_CHANNELS);
+summed_data = sum(shaped_data);
+final = reshape(summed_data,N_CHANNELS,SAMPLES_PER_PACKET);
 
-for chan = 1:N_CHANNELS
-    
-
-
-%Biosemi data is formatted C1S1, C2S1, C3S1, C4S1, C1S2, C2S2 etc. Where
-%C1=Channel 1 and S1 = Sample 1. Each sample is 3 bytes (24-bit), but needs
-%to be padded to 32-bit before converting to an interger in twos complement
-%format.
-
-%offset = ((sample-1))*N_CHANNELS*BYTES_PER_SAMPLE + (chan-1)*BYTES_PER_SAMPLE;
-tic
- %this_sample = sum((bitshift ( stream((offset+1):(offset+3)) , [8;16;24])));
- this_sample = bitshift(stream(offset+1),8) + bitshift(stream(offset+2),16) + bitshift(stream(offset+3),24);
- fprintf('Bitshift %1.5f\n',toc)
- tic
-data(chan,sample) = typecast (uint32(this_sample),'int32');
- fprintf('cast %1.5f\n',toc)
-
-offset = offset + 3;
-        
-
-end
+%Data is received in 2s complement format, convert to 32 bit int.
+for i = 1:size(final,1)
+    data(i,:) = typecast(uint32(final(i,:)),'int32')/(MAGNITUDE_SCALE_FACTOR);
 end
 
